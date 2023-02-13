7f4425b9b543a11dc3095678b8bc75fce521ee97
Ramyamani9975 <ramyamanivannan99@gmail.com>
2023-02-13 17:31:31 +0530

    Add project files.

---
+++ Multithreading/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Multithreading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            threadEx thread =  new threadEx();
+           //thread.hello();
+           //thread.training();
+           Thread thread2 = new Thread(thread.hello);
+           Thread thread3 = new Thread(thread.training);
+
+            thread2.Priority = ThreadPriority.Lowest;
+            thread3.Priority = ThreadPriority.Highest;
+            thread2.Start();
+            thread3.Start();
+            Console.ReadLine();
+
+
+
+        }
+
+    }
+}
---
+++ Multithreading/threadEx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multithreading
+{
+    
+        public class threadEx
+        {
+            public void hello()
+            {
+                Console.WriteLine("Helloworld");
+            
+            }
+
+            public void training()
+            {
+                Console.WriteLine("C# TRAINING");
+
+            }
+
+
+        }
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
